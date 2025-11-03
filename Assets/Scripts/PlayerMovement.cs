936aac9b7707d3de46260954cb3f90a750667df8
wiyuuwiyuu <ayseserraer@gmail.com>
2025-11-03 16:43:03 +0300

    kedy
    i just added the cat asset

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+       
+        
+    }
+}
